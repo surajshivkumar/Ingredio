d6079b6811eb70b27d3097381c16879e9bdb6804
surajshivakumar <surajshivakumar53@gmail.com>
2026-03-21 21:00:35 -0400

    moved mobile to new repo

--- apps/mobile/src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { ScanCard } from './ScanCard';
-export { default as SearchBar } from './SearchBar';
-export { ProductCard } from './ProductCard';
-
-
--- apps/mobile/src/config.ts
+++
@@ -1,4 +0,0 @@
-import { Platform } from 'react-native';
-
-const LOCALHOST = Platform.OS === 'android' ? '10.0.2.2' : 'localhost';
-export const API_URL = `http://${LOCALHOST}:9600/api/v1`;
